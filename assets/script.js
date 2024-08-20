028c354d17762b2eebd1bfc6199ac3994d6abe16
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2024-08-20 16:47:59 -0400

    added JS changes

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,24 @@
 var topics = ["HTML", "CSS", "Git", "Javascript"];
-/*console.log(topics);
-var topic = "HTML";
-if (topic==='HTML'){
-    console.log("Let's study HTML");
-}else if(topic === 'CSS') {
-    console.log("Let's study CSS!");
-}else if(topic === 'GIT') {
-    console.log("Let's study GIT!");
-}else if(topic === 'Javascript') {
-    console.log("Let's study Javascript!");
-}else  {
-    console.log("Please try again later!");
-}
-var shapes = ["triangle","square", "pentagon", "circle" ];
-console.log(shapes[3]);
-*/
-for(var x =0;x < topics.length; x++){
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+ for(var x =0;x < topics.length; x++){
     console.log(topics[x]);
+ }
 }
+function selecttopic(){
+    if (randomTopic==='HTML'){
+        console.log("Let's study HTML");
+    }else if(randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    }else if(randomTopic === 'GIT') {
+         console.log("Let's study GIT!");
+    }else if(randomTopic === 'Javascript') {
+         console.log("Let's study Javascript!");
+    }else  {
+    console.log("Please try again later!");
+    }
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selecttopic();
